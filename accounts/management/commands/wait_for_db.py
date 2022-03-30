8f4c4f6aa62ce1db1058b55d222ac48109086cd4
Mikhail Nikolaev <m.nikolaev1@gmail.com>
2022-03-30 21:11:16 +0300

    Optimized Dockerfile and fixed yml commands

--- accounts/management/commands/wait_for_db.py
+++ accounts/management/commands/wait_for_db.py
@@ -9,8 +9,9 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
-    """Django command to wait for database."""
+    help = """Django command to wait for database."""
 
     def handle(self, *args, **options):
         """Entrypoint for command."""
